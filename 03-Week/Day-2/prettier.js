df141601bc3b9e08051ea1549a5a6cc99c48ea23
enginmertcan <engin.mertcan2002@gmail.com>
2024-09-20 14:04:18 +0300

    chore(eslint): ESLint and Prettier Work Uploaded

---
+++ 03-Week/Day-2/prettier.js
@@ -0,0 +1,41 @@
+
+
+
+// Prettier is a code formatter that automatically formats your code according to a specific style guide. 
+// Its purpose is to standardize the appearance of code so that it looks the same everywhere and eliminates the need for manual editing. Prettier only affects the style of the code, meaning it has no impact on functionality.
+
+// Features:
+
+// Fixes line length.
+// Adjusts indentations (spaces or tabs).
+// Automatically corrects punctuation in code (commas, semicolons, etc.).
+// Does not change the structure of the code, only deals with formatting.
+// Works independently of language (JavaScript, CSS, HTML, etc.).
+// Purpose:
+
+// Eliminates style debates while writing code.
+// Ensures a consistent look throughout the project.
+// ESLint: Code Analysis Tool
+// ESLint is a linter that analyzes JavaScript code and detects errors. It finds potential errors and bad practices in the code. ESLint can check both the style and the correctness of the code.
+
+// Features:
+
+// Analyzes JavaScript code and reports errors.
+// Finds potential errors (e.g., usage of undefined variables).
+// Checks if code adheres to coding standards.
+// Can add style-related rules (e.g., variable naming conventions).
+// You can define your own rules or customize existing ones.
+// Purpose:
+
+// Detect logical errors.
+// Ensure best practices in the code.
+// Prevent performance and security issues.
+
+
+// Differences:
+// Prettier only deals with formatting, while ESLint can check both formatting and logical errors.
+// Prettier automatically adjusts code style; ESLint provides warnings and errors, and manual intervention may be required to fix issues (although it can also provide automatic fixes in some cases).
+// Prettier is simpler and has fixed rules compared to ESLint; ESLint's rules are customizable and can vary by project.
+
+
+// While ESLint detects code errors, Prettier corrects the code's formatting.
